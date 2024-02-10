46f5f3579cbc35067a8dc2b1060675ceaa0248de
User User <stepkadmitrievichreg67@mail.ru>
2024-02-10 20:39:26 +0300

    добавлено сообщение

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,31 +29,55 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(122, 99);
+            this.button1.Location = new System.Drawing.Point(68, 98);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(551, 251);
+            this.button1.Size = new System.Drawing.Size(286, 251);
             this.button1.TabIndex = 0;
             this.button1.Text = "батон";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(465, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(274, 251);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "левак";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(464, 373);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(275, 27);
+            this.textBox1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Button button1;
+        private Button button2;
+        private TextBox textBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace laba2
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "я бебра";
+        }
     }
 }
